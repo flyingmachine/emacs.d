9305d0532959e388fa9f105648c3fc381d49f04f
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-07-30 08:58:15 -0400

    company mode setup

--- customizations/editing.el
+++ customizations/editing.el
@@ -65,3 +65,15 @@
   (mark-whole-buffer)
   (untabify (region-beginning) (region-end))
   (keyboard-quit))
+
+;; Company mode
+(add-hook 'after-init-hook 'global-company-mode)
+(require 'color)
+
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
