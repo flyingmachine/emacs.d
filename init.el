621820dad057d2c70bbfa22ed473eb3e918ae784
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-12-08 09:40:19 -0500

    org update

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
