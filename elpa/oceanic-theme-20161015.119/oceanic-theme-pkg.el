f4e6563a0e6e7271f0e70f22e023dce87de34375
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2017-07-15 10:59:05 -0400

    remove old helm

--- elpa/oceanic-theme-20161015.119/oceanic-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "oceanic-theme" "20161015.119" "Oceanic theme." 'nil :url "https://github.com/terry3/oceanic-theme" :keywords '("oceanic" "color" "theme"))
